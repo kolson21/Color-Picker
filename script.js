be4e7f4fe28b80da73c14c910bdeb330494017cc
Kimberly Olson <kolson21@cnm.edu>
2023-03-31 22:03:41 -0600

    Wrote index and partial script and style.

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+    initColorPicker();
+};
+
+function initColorPicker() {
+    let colorBox = document.getElementsById("color-box")
+    let rgb = {
+        red: document.getElementById("red"),
+        green: document.getElementById("green"),
+        blue: document.getElementById("blue"),
+    };
+}
+
+function setColorPickerEventListeners(colorBox, rgb) {
+    rgb.red.addEventListener('change', () => {
+        console.log("Red value: ", rgb.red.value);
+    })
+}

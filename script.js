6d5a624d84486abcb41eac8bcbca16b3786734cf
Kimberly Olson <kolson21@cnm.edu>
2023-04-02 08:56:34 -0600

    Filled index, script, and style

--- script.js
+++ script.js
@@ -13,15 +13,19 @@ function initColorPicker() {
     setColorPickerEventListeners(colorBox, rgb, colorPickers);
 }
 
-function setColorPickerEventListeners(colorBox, rgb, pickerElements) {
-
-    for (let i=0; i < pickerElements.length; i++) {
+function setColorPickerEventListeners(element, colors, pickerElements) {
+    let pickerLen = pickerElements.length;
+    for (let i=0; i < pickerLen; i++) {
         pickerElements[i].addEventListener('change', () => {
-            setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
+            let red = colors.red.value;
+            let green = colors.green.value;
+            let blue = colors.blue.value;
+            setElementBGColor(element, red, green, blue);
+            setDisplayValues(red, green, blue);
         });
     }
 
-    rgb.red.addEventListener('change', () => {
+    /* rgb.red.addEventListener('change', () => {
         console.log("Red value: ", rgb.red.value);
         setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
     });
@@ -32,10 +36,20 @@ function setColorPickerEventListeners(colorBox, rgb, pickerElements) {
     rgb.blue.addEventListener('change', () => {
         console.log("Blue value: ", rgb.blue.value);
         setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
-    });
+    }); */
 }
 
-function setBoxBGColor(colorBox, red, green, blue) {
-    rgbVal = [red, green, blue].join(',');
-    colorBox.style.backgroundColor = "rgb(" + rgbVal + ")";
+function setElementBGColor(bgElement, red, green, blue) {
+    let rgbVal = [red, green, blue].join(',');
+    bgElement.style.backgroundColor = "rgb(" + rgbVal + ")";
+}
+
+function setDisplayValues(red, green, blue) {
+    let redVal = document.getElementById("redVal");
+    let greenVal = document.getElementById("greenVal");
+    let blueVal = document.getElementById("blueVal");
+
+    redVal.innerText = red;
+    greenVal.innerText = green;
+    blueVal.innerText = blue;
 }

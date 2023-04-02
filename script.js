873e4c6ad7614640640ef9db4a98ba4f88a325b9
Kimberly Olson <kolson21@cnm.edu>
2023-04-01 22:47:05 -0600

    Wrote index and partial script and style.

--- script.js
+++ script.js
@@ -3,16 +3,29 @@ window.onload = function () {
 };
 
 function initColorPicker() {
-    let colorBox = document.getElementsById("color-box")
+    let colorBox = document.getElementById("color-box")
     let rgb = {
         red: document.getElementById("red"),
         green: document.getElementById("green"),
         blue: document.getElementById("blue"),
     };
+    setColorPickerEventListeners(colorBox, rgb);
 }
 
 function setColorPickerEventListeners(colorBox, rgb) {
     rgb.red.addEventListener('change', () => {
         console.log("Red value: ", rgb.red.value);
-    })
+        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
+    });
+    rgb.green.addEventListener('change', () => {
+        console.log("Green value: ", rgb.green.value);
+    });
+    rgb.blue.addEventListener('change', () => {
+        console.log("Blue value: ", rgb.blue.value);
+    });
+}
+
+function setBoxBGColor(colorBox, red, green, blue) {
+    rgbVal = [red, green, blue].join(',');
+    colorBox.style.backgroundColor = "rgb(" + rgbVal + ")";
 }

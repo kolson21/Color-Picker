15ceed6458e2d13bcd84081d6e1214f5573ec78a
Kimberly Olson <kolson21@cnm.edu>
2023-04-01 23:03:47 -0600

    fixed error

--- script.js
+++ script.js
@@ -3,25 +3,35 @@ window.onload = function () {
 };
 
 function initColorPicker() {
-    let colorBox = document.getElementById("color-box")
+    let colorBox = document.getElementById("color-box");
     let rgb = {
         red: document.getElementById("red"),
         green: document.getElementById("green"),
         blue: document.getElementById("blue"),
     };
-    setColorPickerEventListeners(colorBox, rgb);
+    let colorPickers = document.getElementsByClassName("picker");
+    setColorPickerEventListeners(colorBox, rgb, colorPickers);
 }
 
-function setColorPickerEventListeners(colorBox, rgb) {
+function setColorPickerEventListeners(colorBox, rgb, pickerElements) {
+
+    for (let i=0; i < pickerElements.length; i++) {
+        pickerElements[i].addEventListener('change', () => {
+            setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
+        });
+    }
+
     rgb.red.addEventListener('change', () => {
         console.log("Red value: ", rgb.red.value);
         setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
     });
     rgb.green.addEventListener('change', () => {
         console.log("Green value: ", rgb.green.value);
+        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
     });
     rgb.blue.addEventListener('change', () => {
         console.log("Blue value: ", rgb.blue.value);
+        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
     });
 }
 
